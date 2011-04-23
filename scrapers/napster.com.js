3639ac441663e87d96765fa04bbd843eca571927
jherskowitz <jherskowitz@globallistic.com>
2011-04-23 01:14:01 -0400

    Adding Hype Machine and Soundtracking scrapers

---
+++ scrapers/napster.com.js
@@ -0,0 +1,28 @@
+
+/**
+ * Napster Playgrub Scraper
+ * Created by: Jason Herskowitz <jherskowitz AT globallistic DOT com>
+ * Version: 0.1
+ *
+ * Notes:
+ *
+ * This scraper will work on Napster playlist pages.
+ */
+Playgrub.source.url = 'http://.*\.napster.com/playlist.htm.*';
+Playgrub.source.error = 'Sorry, no tracks were found.'
+Playgrub.source.scrape = function() {
+
+    $(".track_info").each(function() {
+        var artist = $(this).find('a:last-child').text();
+        var song = $(this).find('a:second-child').text();
+
+        artist = $.trim(artist);
+        song = $.trim(song);
+
+        if (artist && song) {
+            Playgrub.playlist.add_track(artist, song);
+        }
+    });
+}
+
+Playgrub.source.start();
---
+++ scrapers/soundtracking.com.js
@@ -0,0 +1,24 @@
+
+/**
+ * Soundtracking Tomahawklet Scraper
+ * Created by: Jason Herskowitz <jherskowitz AT globallistic DOT com>
+ * Version: 0.1
+ *
+ * Notes:
+ *
+ * This scraper will work on Hype Machine pages.
+ */
+Playgrub.source.url = 'http://.*\.soundtracking.com/tracks.*';
+Playgrub.source.error = 'Sorry, no tracks were found.'
+Playgrub.source.scrape = function() {
+
+    $(".player").each(function() {
+        var artist = $(this).find('p.artist').text();
+        var song = $(this).find('p.song').text();
+	artist = artist.replace('by', '');
+
+       Playgrub.playlist.add_track(artist, song);
+    });
+}
+
+Playgrub.source.start();
