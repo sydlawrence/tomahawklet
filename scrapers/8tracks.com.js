c1a1d481e27d675f4539c453443a1802f8fd690f
jherskowitz <jherskowitz@globallistic.com>
2011-05-18 09:49:35 -0400

    Half working versions of some new scrapers

--- scrapers/8tracks.com.js
+++ scrapers/8tracks.com.js
@@ -1,20 +1,20 @@
 
 /**
- * Play by AOL Tomahawklet Scraper
+ * 8tracks Tomahawklet Scraper
  * Created by: Jason Herskowitz <jherskowitz AT globallistic DOT com>
  * Version: 0.1
  *
  * Notes:
  *
- * This scraper will work on Play by AOL song pages.
+ * This scraper will work on 8tracks mix pages.
  */
-Playgrub.source.url = 'http://.*\.playbyaol.com/feed/view.*';
+Playgrub.source.url = 'http://.*\.8tracks.com.*';
 Playgrub.source.error = 'Sorry, no tracks were found.'
 Playgrub.source.scrape = function() {
 
-    $("div.song_info").each(function() {
-        var artist = $(this).find('h2').text();
-        var song = $(this).find('h3').text();
+    $('div.playlist').each(function() {
+        var artist = $(this).find('span.a').text();
+        var song = $(this).find('span.t').text().trim();
 	
 
        Playgrub.playlist.add_track(artist, song);
