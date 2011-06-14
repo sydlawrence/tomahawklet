641330802911e6eacf02e01d931d08186600aa25
Matthias Gutjahr <mattsches@gmail.com>
2011-06-14 23:14:05 +0200

    Enabled multiple items within haudio

--- scrapers/default.js
+++ scrapers/default.js
@@ -1,23 +1,44 @@
 /**
  * haudio Playgrub Scraper
  * Created by: JP Hastings-Spital
- * Version: 0.1
+ * Version: 0.2
  *
  * Notes:
  *
  * To test, go to http://www.classicfm.co.uk/on-air/playlist/
  * and pick a recent show.
+ * To test multiple items, go to http://www.numblog.de/archives/824-NUM-Radioshow-013.html
  */
+(function ($) {
+    $.fn.haudio = function () {
+        var audio = {};
+        audio.artist = this.find(".contributor .fn:first").text();
+        audio.title = this.find(".title").text();
+        if (!audio.title) {
+            audio.title = this.find("span.fn").text();
+        }
+        return audio;
+    };
+}(jQuery));
+
 Playgrub.source.url = '.*';
 Playgrub.source.error = 'Sorry, no suitable haudio tags could be found on this page';
-Playgrub.source.scrape = function() {
+Playgrub.source.scrape = function () {
     $(".haudio").each(function () {
-		var artist = $(this).find("span.contributor").text();
-		var title = $(this).find("span.title").text();
-        title = title?title:$(this).find("span.fn").text();
-		if( artist && title && artist != "" && title != "" )
-	    	Playgrub.playlist.add_track(artist,title);
+        var item = $(this).find('.item');
+        if (item.length > 0) {
+            item.each(function () {
+                var track = $(this).haudio();
+                if (track.artist && track.title && track.artist !== "" && track.title !== "") {
+                    Playgrub.playlist.add_track(track.artist, track.title);
+                }
+            });
+        } else {
+            var track = $(this).haudio();
+            if (track.artist && track.title && track.artist !== "" && track.title !== "") {
+                Playgrub.playlist.add_track(track.artist, track.title);
+            }
+        }
     });
 }
-
 Playgrub.source.start();
