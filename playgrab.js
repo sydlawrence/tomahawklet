61005ebefddd54457f3ea3759ff169026263fe4f
Syd Lawrence <sydlawrence@gmail.com>
2012-06-17 23:35:19 +0100

    scraper tweak

--- playgrab.js
+++ playgrab.js
@@ -161,7 +161,9 @@ Playgrab = {
 			position:"absolute",
 			top:0,
 			left:0,
-			zIndex:-1
+			zIndex:-1,
+			width:"100%",
+			height:"100%"
 		});
 		var img = li.find("img");
 		img.css({opacity:0});
