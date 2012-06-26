05a7190e674b6786145447a77cce89c2a8575dce
Syd Lawrence <sydlawrence@gmail.com>
2012-06-26 23:04:39 +0100

    bug fixes

--- playgrab.js
+++ playgrab.js
@@ -294,7 +294,7 @@ Playgrab = {
 		li.find('a').click(function(e) {
 			e.preventDefault();
 			$('.tomahk-iframe').remove();
-			var iframe = $("<iframe style='border:none;position:fixed;top:30px;left:0;z-index:9999999;' class='tomahk-iframe' width=274 height=274 src='http://stage.toma.hk/embed.php?artist="+escape(artist)+"&title="+escape(title)+"&autoplay=true'></iframe>");
+			var iframe = $("<iframe style='border:none;position:fixed;top:30px;left:0;z-index:9999999;' class='tomahk-iframe' width=274 height=274 src=''></iframe>");
 			var closer = $("<span class='tomahk-iframe'>x</span>");
 			closer.css({
 				position:"fixed",
@@ -311,10 +311,14 @@ Playgrab = {
 			}).css("font-family", "Helvetica, arial, sans-serif");
 			iframe.css({borderRight:"1px solid #f00"});
 		
+			var url = "http://stage.toma.hk/embed.php?artist="+escape(artist)+"&title="+escape(title)+"&autoplay=true";
+
 			that.div.animate({top:304});
 			closer.click(function() {
+
 				$('.tomahk-iframe').remove();
 				that.div.animate({top:30});
+				iframe.attr("src", url);
 			});
 			$('body').append(iframe).append(closer);
 		})
