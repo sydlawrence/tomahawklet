114afe6a99b0d381d30bbeb848c7f5925a0d112f
Syd Lawrence <sydlawrence@gmail.com>
2012-06-26 22:25:05 +0100

    made npr work

--- scrapers/npr.com.js
+++ scrapers/npr.com.js
@@ -19,7 +19,7 @@ if(Playgrub.source.set_url('npr.org')) {
         $('.playlistwrap tr:not(.tableHeader)').each(function(){
             var title = $(this).find(".song").html();
             var artist = $(this).find(".artist").html();
-            Playgrab.insertAddButton($(this),artist,title);
+   			Playgrub.playlist.add_track(artist,title);
 
         })
     }
