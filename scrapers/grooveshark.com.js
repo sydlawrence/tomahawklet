9027533a8e1f3373167c29d6a5863094208dd985
Syd Lawrence <sydlawrence@gmail.com>
2012-06-27 15:15:51 +0100

    bug fixes

--- scrapers/grooveshark.com.js
+++ scrapers/grooveshark.com.js
@@ -11,9 +11,10 @@
 Playgrub.source.url = 'grooveshark.com';
 Playgrub.source.error = "To use Tomahawklet on Grooveshark export a song list, choose \"Mirror Changes\" or \"Copy Songs\", scroll down and hit \"Next Step\", then use the bookmarklet.";
 Playgrub.source.scrape = function() {
-    $("h4").each(function () {
-        var song_result = $(this).html().split(" - ");
-        Playgrub.playlist.add_track(song_result[1], song_result[0]);
+    $(".slick-row").each(function () {
+    	var artist = $(this).find(".artist a").html();
+    	var title = $(this).find(".songName a").html();
+        Playgrub.playlist.add_track(artist, title);
     });
 }
 
