0f7364beb02a3cfd9119083b32dead8eaea01d10
Syd Lawrence <sydlawrence@gmail.com>
2012-06-27 15:07:28 +0100

    bug fixes

--- scrapers/everyhit.com.js
+++ scrapers/everyhit.com.js
@@ -10,7 +10,7 @@
 Playgrub.source.url = 'http://www.everyhit.com/*';
 Playgrub.source.error = 'Sorry, there aren\'t any appropriate song names here.';
 Playgrub.source.scrape = function() {
-	$("table[width=600] tr").each(function () {
+	$("#retrochart] tr").each(function () {
 		if ($(this).find('td').length == 3) {
 			var artist = $(this).find('td').eq(1).text();
 			var title = $(this).find('td').eq(2).text();
