bdd19c897e08c73062564cc437184ad66e547c1f
Syd Lawrence <sydlawrence@gmail.com>
2012-06-27 16:24:03 +0100

    Added soundcloud embed

---
+++ scrapers/soundcloudembed.js
@@ -0,0 +1,64 @@
+
+
+/**
+ * Soundcloud embeds Tomahawklet Scraper
+ * Created by: Jason Herskowitz <jherskowitz AT globallistic DOT com>
+ * Version: 0.1
+ *
+ * Notes:
+ *
+ * This scraper will work on all pages with spotify embeds
+ */
+Playgrub.source.url = '*';
+Playgrub.source.error = 'Sorry, no tracks were found. Make sure you are on the "tracks" tab.'
+Playgrub.source.scrape = function() {
+
+
+	processPlaylist = function(data) {
+		alert("we need to process the soundcloud playlists");
+	}
+
+	processTrack = function(track) {
+
+	}
+
+    $('iframe').each(function() {
+    	var src = $(this).attr("src");
+
+    	var regex = new RegExp("w.soundcloud.com/player");
+        if(regex.exec(src)) {
+        	src = src.split("?");
+        	src = src[1].split("&");
+        	var uri = src[0];
+        	uri = uri.split("=");
+        	uri = uri[1];
+
+        	http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F49453498
+
+        	var url = uri+"?consumer_key=TiNg2DRYhBnp01DA3zNag&format=json&callback=?";
+
+        	var url = "http://spotikea.tomahawk-player.org/browse/"+uri;
+        	url = "http://stage.toma.hk/proxy.php?url="+encodeURIComponent(url)+"&callback=?";
+        	$.getJSON(url, function(data){
+
+        		if (data.kind === "track") {
+
+        			var title = data.title.split(" - ");
+        			var artist = title[0];
+        			title = title[1];
+		    	
+		    		Playgrub.playlist.add_track(artist,title);
+
+        		}
+        	})
+        	
+
+        }
+
+    	//"https://embed.spotify.com/?uri=spotify:user:napstersean:playlist:3vxotOnOGDlZXyzJPLFnm2&theme=white
+    })
+
+    
+}
+
+Playgrub.source.start();
