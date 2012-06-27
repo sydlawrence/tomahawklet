e7b76d0458ea13b359a541fb865094ab7255957e
Syd Lawrence <sydlawrence@gmail.com>
2012-06-27 15:11:16 +0100

    bug fixes

--- scrapers/ex.fm.js
+++ scrapers/ex.fm.js
@@ -12,9 +12,9 @@ Playgrub.source.url = 'http://.*.ex.fm.*';
 Playgrub.source.error = 'Sorry, no tracks were found.'
 Playgrub.source.scrape = function() {
 
-    $(".songVOListLeft").each(function() {
-        var artist = $(this).find('div.songVOListArtist').text();
-        var song = $(this).find('a.songVOListSongTitle').text();
+    $(".song_row_box").each(function() {
+        var artist = $(this).find('.song_row_artist').text();
+        var song = $(this).find('.song_row_title').text();
 	
 
        Playgrub.playlist.add_track(artist, song);
