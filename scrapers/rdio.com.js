cb1f3ece0bb5826db7d71b082750e45cf313c2cc
Syd Lawrence <sydlawrence@gmail.com>
2012-06-27 15:32:25 +0100

    bug fixes

--- scrapers/rdio.com.js
+++ scrapers/rdio.com.js
@@ -11,28 +11,16 @@ Playgrub.source.url = 'http://.*\.rdio.com.*';
 Playgrub.source.error = 'Check your Rdio page - only artist, album and playlist pages are supported.'
 Playgrub.source.scrape = function() {
 
-    var albumArtist;
-    var ah = $("div.album_header");
-
-    if (ah.size() > 0) {
-        albumArtist = ah.find("a.mini_header").text();
-    }
-
-    $("div.track").each(function() {
-        var artist = $(this).find('div.album_info > a:first-child').text();
-        var song = $(this).find('div.title_info').text();
-
-        if (!artist) {
-            artist = albumArtist;
-        }
-
-        artist = $.trim(artist);
-        song = $.trim(song);
+    $('.Track').each(function(){
+        var artist = $(this).find(".metadata a:nth-child(1)").html();
+        var song = $(this).find(".metadata a:nth-child(2)").html();
 
         if (artist && song) {
             Playgrub.playlist.add_track(artist, song);
-        }
+        }  
     });
+
+    
 }
 
 Playgrub.source.start();
