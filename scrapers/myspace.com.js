06651455ccd7b4b116505c47af754a3ac92110bc
Syd Lawrence <sydlawrence@gmail.com>
2012-06-28 23:31:26 +0100

    added myspace scraper

---
+++ scrapers/myspace.com.js
@@ -0,0 +1,28 @@
+/**
+ * 22tracks Tomahawklet Scraper
+ * Created by: Jason Herskowitz <jherskowitz AT globallistic DOT com>
+ * Version: 0.1
+ *
+ * Notes:
+ *
+ * This scraper will work on 22tracks playlist pages.
+ */
+Playgrub.source.url = 'myspace.com';
+Playgrub.source.error = 'Sorry, no tracks were found.'
+Playgrub.source.scrape = function() {
+
+	$('.songs li').each(function() {
+		var artist = $(this).find(".info h5 a").text();
+		var title = $(this).find(".extra p a").text();
+
+		Playgrub.playlist.add_track(artist, title);
+
+	});
+}
+
+Playgrub.source.start();
+
+
+
+
+		
