5ac36c198b9745010107ff8f79a1f0c5717478d0
Syd Lawrence <sydlawrence@gmail.com>
2012-06-28 22:26:44 +0100

    added new scrapers

--- scrapers/facebook.com.js
+++ scrapers/facebook.com.js
@@ -11,12 +11,16 @@ Playgrub.source.url = 'http://.*\.facebook.com.*';
 Playgrub.source.error = 'Check your Facebook page - only music pages are supported.'
 Playgrub.source.scrape = function() {
 
-$("li.music_song_item").each(function() {
-        var song = $(this).find('a.music_button_trigger').text();
-        var artist = $(this).find('div.uiCollapsedListMiddot span').text();
+	$("li.music_song_item").each(function() {
+	        var song = $(this).find('a.music_button_trigger').text();
+	        var artist = $(this).find('div.uiCollapsedListMiddot span').text();
 
-       Playgrub.playlist.add_track(artist, song);
-    });
-}
+	       Playgrub.playlist.add_track(artist, song);
+	    });
+	}
+
+	$('.pageMenuButton .uiButtonText').each(function(){
+				
+	})
 
 Playgrub.source.start();
