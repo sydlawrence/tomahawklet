21d31e69fd4c1673b97499456e142ed7fa2544b7
Syd Lawrence <sydlawrence@gmail.com>
2012-06-29 17:13:59 +0100

    bug fix

--- playgrab.js
+++ playgrab.js
@@ -326,7 +326,7 @@ Playgrab = {
 	generateForm: function() {
 		this.form = $("<form method='post' target='_blank' action='http://toma.hk/playlistgen.php'/>");
 		this.form.append("<input type='hidden' name='title' value='"+window.document.title+"' />");
-		this.form.append("<input type='image' height=20 src='https://github.com/sydlawrence/tomahawklet/raw/master/playlist-icon.png' alt='Create a playlist from this page' title='Create a playlist from this page' />");
+		this.form.append("<input type='image' height=20 style='border:none;padding:0;margin:0;width:20px;height:20px;' src='https://github.com/sydlawrence/tomahawklet/raw/master/playlist-icon.png' alt='Create a playlist from this page' title='Create a playlist from this page' />");
 
 		this.form.append("<input type='hidden' name='save' value='true' />");
 		this.form.append("<input type='hidden' name='redirect' value='true' />");
