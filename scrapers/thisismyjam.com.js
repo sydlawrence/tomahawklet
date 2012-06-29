bdd00ae5d476d4e77f06b777f4c5aee45f6e5d58
Syd Lawrence <sydlawrence@gmail.com>
2012-06-29 17:07:45 +0100

    fixed this is my jam

--- scrapers/thisismyjam.com.js
+++ scrapers/thisismyjam.com.js
@@ -11,14 +11,13 @@ Playgrub.source.url = 'http://*thisismyjam.com.*';
 Playgrub.source.error = 'Check your This is My Jam page - only playlist pages are supported.'
 Playgrub.source.scrape = function() {
 
-$("div.attachedContent").each(function() {
-        var title = $.trim($(this).find('h4 a.jamTitle').text());
-        var a = title.toString().split("\n");
-        var song = $.trim(a[0]);
-var artist = a[1].replace(/^\s*by|\s*$/g , '');
-artist = $.trim(artist);
+	$(".jamDescription").each(function() {
+		var title = $.trim($(this).find('#jamTitle').text());
+		var artist = $.trim($(this).find('#jamArtist').text());
 
-       Playgrub.playlist.add_track(artist, song);
+		artist = artist.replace("by","");
+		console.log(artist);
+		Playgrub.playlist.add_track(artist, title);
     });
 }
 
