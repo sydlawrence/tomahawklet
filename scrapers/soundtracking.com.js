ae1e5bceaea7702fe383e5ee424a58de3c49689c
Syd Lawrence <sydlawrence@gmail.com>
2012-06-29 18:09:32 +0100

    fixed soundtracking

--- scrapers/soundtracking.com.js
+++ scrapers/soundtracking.com.js
@@ -10,24 +10,22 @@
 Playgrub.source.url = 'soundtracking.com';
 Playgrub.source.error = 'Sorry, no tracks were found.'
 Playgrub.source.scrape = function() {
-if (window.location.href.indexOf("soundtracking.com/") != -1) 
-{ 
-	$('div.song-zone').each(function () {
-		var song = $(this).find('p.post-title').text();
-		var artist = $(this).find('p.artist-name').text();
- 	Playgrub.playlist.add_track(artist, song);
-     });
+	if (window.location.href.indexOf("soundtracking.com/posts/") != -1) {
+		$(".player").each(function() {
+			var artist = $(this).find('p.artist').text();
+			var song = $(this).find('p.song').text();
+			artist = artist.replace('by', '');
 
-} else if (window.location.href.indexOf("soundtracking.com/posts/") != -1) {
+			Playgrub.playlist.add_track(artist, song);
+		});
+	} else if (window.location.href.indexOf("soundtracking.com/") != -1) {
+		$('div.song-zone').each(function() {
+			var song = $(this).find('p.post-title').text();
+			var artist = $(this).find('p.artist-name').text();
+			Playgrub.playlist.add_track(artist, song);
+		});
 
- $(".player").each(function() {
-        var artist = $(this).find('p.artist').text();
-        var song = $(this).find('p.song').text();
-	artist = artist.replace('by', '');
-
-       Playgrub.playlist.add_track(artist, song);
-    });
-}
+	}
 
 };
 
