7d921f6af4da39d0fe326de31b1fa5f3698047d3
Syd Lawrence <sydlawrence@gmail.com>
2012-07-01 18:02:36 +0100

    added extra bit in npr scraper

--- scrapers/npr.org.js
+++ scrapers/npr.org.js
@@ -31,7 +31,7 @@ Playgrub.source.scrape = function() {
     	var title = strip($(this).find('.info li:nth-child(2)').html());
         title = title.replace("Song: ");
     	var artist = $(this).find(".bucketblock h4").text();
-
+		Playgrub.playlist.add_track(artist,title);
     })
 }
 Playgrub.source.start();
