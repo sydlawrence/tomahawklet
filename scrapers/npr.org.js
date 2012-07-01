1179bedda3b0b797a83928bc6bf441c52ecaae70
Syd Lawrence <sydlawrence@gmail.com>
2012-07-01 18:03:12 +0100

    bug fix

--- scrapers/npr.org.js
+++ scrapers/npr.org.js
@@ -29,7 +29,7 @@ Playgrub.source.scrape = function() {
     });
     $('.playlistitem').each(function() {
     	var title = strip($(this).find('.info li:nth-child(2)').html());
-        title = title.replace("Song: ");
+        title = title.replace("Song: ","");
     	var artist = $(this).find(".bucketblock h4").text();
 		Playgrub.playlist.add_track(artist,title);
     })
