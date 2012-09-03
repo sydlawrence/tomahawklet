dc31f8003ef2dcd4c54d5f40a0dd0923537bd87c
Pluto <plutoisaplanet1@gmail.com>
2012-09-03 14:57:10 -0700

    Update scrapers/apple.com.js
    Add support for all other music pages on Apple iTunes website. Not yet sure if it works... Will be testing.

--- scrapers/apple.com.js
+++ scrapers/apple.com.js
@@ -20,3 +20,60 @@ Playgrub.source.scrape = function() {
 }
 
 Playgrub.source.start();
+
+/**
+* iTunes Song/Artist/Playlist Scraper
+* Created by: Daniel Nunes
+* Version: 0.1
+*
+* Notes:
+*
+* This scraper will work on itunes.apple.com Song, Music Artist, and Playlist pages.
+*/
+Playgrub.source.url = 'itunes.apple.com';
+Playgrub.source.error = 'Tomahawk currently supports iTunes song, playlist, and music artist pages.'
+Playgrub.source.scrape = function() {
+    var page = document.URL;
+	/* Gets the page URL */
+	
+	if(page.lastIndexOf('http://',0) == 0) /* "lastIndexOf(string, startIndex) == 0" sees if string starts with that parameter given */
+		page = page.substring(7);
+	else if(page.lastIndexOf('https://',0) == 0)
+		page = page.substring(8);
+	else
+		return;
+	/* Continues if http or https and removes URL protocol */
+	
+	if(page.lastIndexOf('itunes.apple.com/',0) == 0)
+		page = page.substring(page.indexOf('/',17)+1);
+	else
+		return;
+	/* Continues if the URL begins with itunes.apple.com, and removes the domain + locale of URL */
+	
+	if(page.lastIndexOf('album',0) == -1 && page.lastIndexOf('playlist',0) && -1 && page.lastIndexOf('artist',0) && -1)
+		return;
+	/* Only works on album, playlist, and artist pages */
+	
+	var a = document.getElementsByClassName('track-list');
+	if(a.length == 0)
+		return;
+	for(var b = 0; b < a.length; b++) {
+		if(a[b].offsetWidth != '0') {
+			a = a[b].getElementsByClassName('song music');
+			break;
+		}
+		else if(a.length-1 == b)
+			return;
+	}
+	/* Gets the first track-list that's visible */
+	
+	var output = '';
+	for(var b = 0; b < a.length; b++) {
+		var artist = a[b].getAttribute('preview-artist');
+		var song = a[b].getAttribute('preview-title');
+		if(artist != null && song != null)
+			Playgrub.playlist.add_track(artist,song);
+	}
+}
+
+Playgrub.source.start();
